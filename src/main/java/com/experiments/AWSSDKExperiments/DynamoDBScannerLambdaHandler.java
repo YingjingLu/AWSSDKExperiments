6d7dda8eb39e662210880e8a813f7ac252fe6ac6
YingjingLu <yingjinglu2019us@gmail.com>
2021-02-28 10:49:35 -0500

    d

--- src/main/java/com/experiments/AWSSDKExperiments/DynamoDBScannerLambdaHandler.java
+++ src/main/java/com/experiments/AWSSDKExperiments/DynamoDBScannerLambdaHandler.java
@@ -1,8 +1,32 @@
 package com.experiments.AWSSDKExperiments;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
+import com.google.gson.GsonBuilder;
+import com.google.gson.Gson;
+import java.util.Map;
 
-public class DynamoDBScannerLambda {
+public class DynamoDBScannerLambdaHandler implements RequestHandler<Map<String,String>, String>{
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    @Override
+    public String handleRequest(Map<String,String> event, Context context)
+    {
+        LambdaLogger logger = context.getLogger();
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(Regions.US_WEST_2)
+                .build();
 
+        String response = new String("200 OK");
+        // log execution details
+        logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
+        logger.log("CONTEXT: " + gson.toJson(context));
+        // process event
+        logger.log("EVENT: " + gson.toJson(event));
+        logger.log("EVENT TYPE: " + event.getClass().toString());
+        return response;
+    }
 }
