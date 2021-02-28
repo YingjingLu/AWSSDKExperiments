fce78e9a48e86a60636fc87d5c42fd8ad94a5d07
YingjingLu <yingjinglu2019us@gmail.com>
2021-02-27 21:54:53 -0500

    f

---
+++ src/main/java/com/experiments/AWSSDKExperiments/DynamoDBScannerLambda.java
@@ -0,0 +1,8 @@
+package com.experiments.AWSSDKExperiments;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+public class DynamoDBScannerLambda {
+
+}
