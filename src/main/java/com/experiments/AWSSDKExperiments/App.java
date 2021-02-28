3a3c1a18aa2c8a89aabe9f8f04f3ec13d0789f82
YingjingLu <yingjinglu2019us@gmail.com>
2021-02-27 20:33:50 -0500

    initial commit

---
+++ src/main/java/com/experiments/AWSSDKExperiments/App.java
@@ -0,0 +1,13 @@
+package com.experiments.AWSSDKExperiments;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
